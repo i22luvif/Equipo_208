df7d2534c86050595d62a7cb7235e6e11d8f44b0
i22luvif <150723476+i22luvif@users.noreply.github.com>
2023-12-14 19:17:31 +0100

    Segundo Commit

--- include/actividad.h
+++ include/actividad.h
@@ -15,7 +15,7 @@ class Actividad {
 private:
     string nombre;
     string descripcion;
-    string ponente;  // Nuevo atributo
+    string ponente;  
     string fecha_hora;
     string ubicacion;
     int plazas_disponibles;
@@ -24,6 +24,7 @@ private:
 
 public:
     // Constructor de la clase
+    Actividad() = default;
     Actividad(string nom, string desc, string pon, string fecha, string ubic, int plazas, string cat, double prec)
         : nombre(nom), descripcion(desc), ponente(pon), fecha_hora(fecha), ubicacion(ubic), plazas_disponibles(plazas), categoria(cat), precio(prec) {}
     
@@ -46,22 +47,13 @@ public:
     // Método estático para mostrar actividades desde un archivo
     static vector<Actividad> obtenerDetalles(const string& nombreArchivo);
     
-    // Método estático para crear una nueva actividad
-    static Actividad crearActividad();
-
     // Método estático para guardar una actividad en un archivo
     static void guardarActividad(const Actividad& nuevaActividad, const string& nombreArchivo);
-
-    // Método estático para editar una actividad
-    static void editarActividad(vector<Actividad>& actividades);
-
-    // Método estático para eliminar una actividad
-    static void eliminarActividad(vector<Actividad>& actividades);
 
     // Método estático para mostrar las personas inscritas en una actividad
     static void mostrarInscritos(const string& nombreActividad);
 };
 
 
+
 #endif
-int menu_actividad();
